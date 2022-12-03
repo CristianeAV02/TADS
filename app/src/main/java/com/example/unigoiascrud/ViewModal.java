e225d08967fb9923c60632dccd47640545761fd2
CristianeAV02 <119754779+CristianeAV02@users.noreply.github.com>
2022-12-03 16:13:54 -0300

    CRUD

---
+++ app/src/main/java/com/example/unigoiascrud/ViewModal.java
@@ -0,0 +1,30 @@
+package com.example.unigoiascrud;
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import java.util.List;
+public class ViewModal extends AndroidViewModel {
+    private CourseRepository repository;
+    private LiveData<List<CourseModal>> allCourses;
+    public ViewModal(@NonNull Application application) {
+        super(application);
+        repository = new CourseRepository(application);
+        allCourses = repository.getAllCourses();
+    }
+    public void insert(CourseModal model) {
+        repository.insert(model);
+    }
+    public void update(CourseModal model) {
+        repository.update(model);
+    }
+    public void delete(CourseModal model) {
+        repository.delete(model);
+    }
+    public void deleteAllCourses() {
+        repository.deleteAllCourses();
+    }
+    public LiveData<List<CourseModal>> getAllCourses() {
+        return allCourses;
+    }
+}
